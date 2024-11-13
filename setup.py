14a9f03cc56da9648e9d09ec94ca9ffc59d19cbc
Opticz <cartiscott45@gmail.com>
2024-11-13 17:01:41 +0000

    Update setup.py with correct license expression

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ setup(
     url='https://github.com/Opticz45/pypasslib', 
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: Creative Commons Attribution-NonCommercial 4.0 International License',
         'Operating System :: OS Independent',
     ],
+    license_expression='CC-BY-NC-4.0',
     python_requires='>=3.6',
 )
